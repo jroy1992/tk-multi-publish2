99e869fb2e7ab158ba17c5a5b7374a1395050756
ssreeram <ssreeram@d2.com>
2019-11-21 15:18:05 +0530

    #3141: modified code to check key existence before removing

--- hooks/houdini/collector.py
+++ hooks/houdini/collector.py
@@ -393,7 +393,7 @@ class HoudiniSessionCollector(HookBaseClass):
 
         # also collect sequence files
         for key in self.sgtk.template_keys.values():
-            if isinstance(key, SequenceKey):
+            if isinstance(key, SequenceKey) and key.name in fields:
                 fields.pop(key.name)
 
         work_tmpl = publisher.get_template_by_name(work_path_template)
